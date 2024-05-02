d854e71c02174b16175c7028f599933998ddaa5b
ksherald17 <ksherald17@naver.com>
2024-05-03 02:53:40 +0900

    Add other models

--- SCDLSimpleTest.py
+++ SCDLSimpleTest.py
@@ -75,8 +75,9 @@ def soft_label_cross_entropy(preds, soft_labels, true_labels, confidence_mask):
     if true_labels.dim() > 1:
         true_labels = torch.argmax(true_labels, dim=-1)  # Convert one-hot encoded labels to class indices
 
-    while true_labels.dim() < preds.dim()-1:
-        true_labels = torch.unsqueeze(true_labels, -1)
+    # while true_labels.dim() < preds.dim()-1:
+    #     true_labels = torch.unsqueeze(true_labels, -1)
+    true_labels = true_labels.unsqueeze(1).expand(-1, preds.size(1))
 
     # Calculate the soft label loss using KL divergence
     soft_label_loss = F.kl_div(F.log_softmax(preds, dim=-1), soft_labels, reduction='none').sum(dim=-1)
